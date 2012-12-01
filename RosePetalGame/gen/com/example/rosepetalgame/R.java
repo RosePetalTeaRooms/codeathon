42a536d7220bdf040ade19ac431432a320f50e88
Gillies <Gillies@192.168.0.2>
2012-12-01 19:54:22 +0000

    menu screen now only landscape
    changed background music
    
    - Gil

--- RosePetalGame/gen/com/example/rosepetalgame/R.java
+++ RosePetalGame/gen/com/example/rosepetalgame/R.java
@@ -11,13 +11,16 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int fish6=0x7f020000;
-        public static final int ic_action_search=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int background=0x7f020000;
+        public static final int background2=0x7f020001;
+        public static final int fish6=0x7f020002;
+        public static final int ic_action_search=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
-        public static final int button=0x7f080000;
-        public static final int menu_settings=0x7f080001;
+        public static final int button=0x7f080001;
+        public static final int imageView=0x7f080000;
+        public static final int menu_settings=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -28,12 +31,14 @@ public final class R {
     }
     public static final class raw {
         public static final int background=0x7f040000;
-        public static final int drop=0x7f040001;
-        public static final int win=0x7f040002;
+        public static final int bgmusic=0x7f040001;
+        public static final int drop=0x7f040002;
+        public static final int win=0x7f040003;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
         public static final int game_name=0x7f050004;
+        public static final int game_resume=0x7f050006;
         public static final int menu_settings=0x7f050002;
         public static final int start_game=0x7f050001;
         public static final int title_activity_main=0x7f050003;
--- RosePetalGame/src/com/example/rosepetalgame/ScreenActivity.java
+++ RosePetalGame/src/com/example/rosepetalgame/ScreenActivity.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
@@ -15,6 +17,9 @@ public class ScreenActivity extends Activity {
 	 @Override
 	    public void onCreate(Bundle savedInstanceState) 
 	    {
+		 requestWindowFeature(Window.FEATURE_NO_TITLE);
+	       getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+	        					WindowManager.LayoutParams.FLAG_FULLSCREEN);
 	    	
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.activity_menu); 
